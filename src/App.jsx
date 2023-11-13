f3f107b213cd7785a98f9529d2f30f3fbd492286
Tak <takeshiseodev@gmail.com>
2023-11-13 17:48:03 -0500

    Step 2: Dynamic Box Styling Based on ID Parity

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@
 import { useState } from 'react'
 import boxes from './boxes'
 
-function App() {
+function App(props) {
     const [squares, setSquares] = useState(boxes)
 
-    const squareElements = squares.map(square => <div key={square.id} className="box"></div>)
+    const squareElements = squares.map(square => {
+        const styles = {
+            backgroundColor: square.id % 2 === 0 ? 'red' : 'white',
+        }
+
+        return <div key={square.id} className="box" style={styles}></div>
+    })
 
     // const squareElements = squares.map(function(square) {
     //   return <div key={square.id} className='box'></div>
