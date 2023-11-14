da725f1d80920e4e73af0024617a19ee990db1fa
Tak <takeshiseodev@gmail.com>
2023-11-13 22:50:28 -0500

    update: step 6
    fixed missing ! (oops)

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,21 @@ function App() {
     const [squares, setSquares] = useState(boxes)
 
     function toggle(id) {
-        console.log('clicked!')
+        setSquares(prevSquares => {
+            const updatedSquaresArray = []
+          for (let i = 0; i < prevSquares.length; i++) {
+              const currentSquare = prevSquares[i]
+                if (currentSquare.id === id) {
+                    updatedSquaresArray.push({
+                        ...currentSquare,
+                        on: !currentSquare.on,
+                    })
+                } else {
+                    updatedSquaresArray.push(currentSquare)
+                }
+            }
+            return updatedSquaresArray
+        })
     }
 
     const squareElements = squares.map(square => (
