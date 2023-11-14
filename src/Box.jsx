2fe59c56650970b40ae44c193a2a8cd85c3ce1f6
Tak <takeshiseodev@gmail.com>
2023-11-13 19:55:27 -0500

    update: Implement Local State in Box Component (step4)

--- src/Box.jsx
+++ src/Box.jsx
@@ -3,11 +3,19 @@
 import React from 'react'
 
 function Box(props) {
+
+    const [on, setOn] = React.useState(props.on)
+
     const styles = {
-        backgroundColor: props.on ? '#222222' : 'transparent',
+        backgroundColor: on ? '#222222' : 'transparent',
     }
+
+    function toggle() {
+        setOn(prevOn => !prevOn)
+    }
+
     return (
-        <div className="box" style={styles}></div>
+        <div className="box" style={styles} onClick={toggle} ></div>
     )
 }
 
